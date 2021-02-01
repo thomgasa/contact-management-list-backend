741bcbcfd2c8ae4d39cd120daf7c38a2e8a07edd
thomgasa <thomasjgamboas@gmail.com>
2021-02-01 14:00:40 -0400

    Finished exercise

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, Contact
 #from models import Person
 
 app = Flask(__name__)
@@ -30,14 +30,68 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
-
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
-
+@app.route('/contacts', methods=['GET'])
+def handle_contact():
+    contacts = Contact.query.all()
+    response_body = []
+    for contact in contacts:
+        response_body.append(contact.serialize())
     return jsonify(response_body), 200
+
+@app.route('/contacts', methods=['POST'])
+def add_new_contact():
+    body = request.get_json()
+    print (body)
+    new_contact = Contact(
+        full_name = body["full_name"],
+        email = body["email"],
+        address = body["address"],
+        phone = body["phone"],
+    )
+    db.session.add(new_contact)
+    try:
+        db.session.commit()
+        print (new_contact.serialize())
+        return jsonify (new_contact.serialize()), 201
+    except Exception as error:
+        print (error.args)    
+        return jsonify("NOT OK"), 500
+
+@app.route('/contacts/<int:id>', methods=['DELETE'])
+def remove_contact(id):
+    contact = Contact.query.get(id)
+    if contact is None:
+        raise APIException('User not found', status_code=404)
+    db.session.delete(contact)
+    db.session.commit()
+    return jsonify([]), 204
+
+@app.route('/contacts/<int:id>', methods=['GET','PUT', 'PATCH'])
+def update_contact(id):
+    if request.method == 'GET':
+        contact= Contact.query.get(id)
+        return jsonify(contact.serialize()), 200
+
+    if request.method == 'PUT':
+        contact= Contact.query.get(id)
+        body= request.get_json()
+        contact.full_name= body['full_name']
+        contact.address= body['address']
+        contact.phone= body['phone']
+        db.session.commit()
+        return jsonify(contact.serialize()), 200
+
+    if request.method == 'PATCH':
+        contact= Contact.query.get(id)
+        body= request.get_json()
+        if "full_name" in body:
+            contact.full_name= body['full_name']
+        if "address" in body:
+            contact.address= body['address']
+        if "phone" in body:
+            contact.phone= body['body']
+            db.session.commit()
+        return jsonify(contact.serialize()), 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -2,18 +2,34 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+#class User(db.Model):
+    #id = db.Column(db.Integer, primary_key=True)
+    #email = db.Column(db.String(120), unique=True, nullable=False)
+    #password = db.Column(db.String(80), unique=False, nullable=False)
+    #is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+    #def __repr__(self):
+        #return '<User %r>' % self.username
+
+    #def serialize(self):
+        #return {
+            #"id": self.id,
+            #"email": self.email,
+            # do not serialize the password, its a security breach
+        #}
+
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(30), nullable=False)
+    email = db.Column(db.String(30), unique=True, nullable=False)
+    address = db.Column(db.String(30), nullable=False)
+    phone = db.Column(db.String(30), nullable=False)
 
     def serialize(self):
         return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "id":self.id,
+            "full_name":self.full_name,
+            "email":self.email,
+            "address":self.address,
+            "phone":self.phone,
         }
